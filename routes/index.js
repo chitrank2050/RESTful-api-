0bd9c8ea92e75b49c10e212f833f2cb819f25ab2
Chitrank Agnihotri <chitrank2050@gmail.com>
2016-12-29 12:52:44 +0530

    ;

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '-Api-',author: 'Chitrank Agnihotri',demo:'/api/chitrank/demo'});
+  res.render('index', { title: '-Api-',author: 'Chitrank Agnihotri',demo:'/api/chitrank/demo',link:'/api/chitrank/demo/5864b015c95caf00201fb7aa'});
 });
 
 module.exports = router;
